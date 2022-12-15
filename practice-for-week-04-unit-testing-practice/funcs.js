4f7b3323f758d5614fcbbe8b385475ccf757148b
jagLas <jglasher@gmail.com>
2022-12-15 15:05:14 -0500

    finished function part

--- practice-for-week-04-unit-testing-practice/funcs.js
+++ practice-for-week-04-unit-testing-practice/funcs.js
@@ -1,13 +1,31 @@
 function isFive(num) {
-  // Your code here
+  if (num === 5) {
+    return true;
+  }
+  return false;
 }
 
 function isOdd(number) {
-  // Your code here
+  if (typeof number !== 'number') {
+    throw Error('Input is not a number');
+  }
+
+  if (number % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  let newNums = [];
+
+  while (min <= max) {
+    newNums.push(min);
+    min += step;
+  }
+
+  return newNums;
 }
 
 
