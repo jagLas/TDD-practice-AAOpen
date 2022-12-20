1c9cd9749aff8109057e5fe8c8817715cef93a73
jagLas <jglasher@gmail.com>
2022-12-20 11:29:55 -0500

    finished maximum length error

--- practice-for-week-04-custom-error-types/error-types/maximum-length-exceeded-error.js
+++ practice-for-week-04-custom-error-types/error-types/maximum-length-exceeded-error.js
@@ -1,6 +1,22 @@
 const ValidationError = require('./validation-error');
 
-// Your code here
+class MaximumLengthExceededError extends ValidationError {
+  constructor(excessLength, ...params) {
+    super(...params);
+
+    if (!excessLength) {
+      this.message = 'Maximum length exceeded';
+    } else {
+      this.message = `Maximum length exceeded by ${excessLength}`;
+    }
+
+    this.name = 'MaximumLengthExceededError';
+
+    // if(Error.captureStackTrace) {
+    //   Error.captureStackTrace(this, MaximumLengthExceededError);
+    // }
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- practice-for-week-04-custom-error-types/error-types/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-passwordInput = new Field('Password', 10);
-// // Field { inputName: 'Password', value: 10 }
-// if (typeof passwordInput.value !== 'string') {
-//   throw new ValidationError();
-// }
