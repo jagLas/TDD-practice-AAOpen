699ae69bef185120ca5dbcc8839f4658fbcb16a1
jagLas <jglasher@gmail.com>
2022-12-20 11:17:37 -0500

    validation error done

---
+++ practice-for-week-04-custom-error-types/error-types/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+passwordInput = new Field('Password', 10);
+// // Field { inputName: 'Password', value: 10 }
+// if (typeof passwordInput.value !== 'string') {
+//   throw new ValidationError();
+// }
--- practice-for-week-04-custom-error-types/error-types/validation-error.js
+++ practice-for-week-04-custom-error-types/error-types/validation-error.js
@@ -1,4 +1,16 @@
-// Your code here
+class ValidationError extends Error {
+  constructor (...params) {
+    super(...params);
+
+    this.name = 'ValidationError';
+    this.message = this.message || 'Invalid input';
+
+    if(Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError);
+    }
+
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
